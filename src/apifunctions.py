c5c32b80ced94b919deb916959941a3caae2bf05
Carlosleono <cleonortiz@gmail.com>
2021-11-15 16:40:45 +0100

    Mongo con Index

--- src/apifunctions.py
+++ src/apifunctions.py
@@ -1,7 +1,8 @@
 import os
 import requests
 from dotenv import load_dotenv
-
+from functools import reduce
+import operator
 
 def foursquare(latitude, longitude, category, radius):
     
@@ -41,3 +42,18 @@ def foursquarequery(latitude, longitude, kueri, radius):
     resp = requests.get(url_query, params = parametros).json()
 
     return resp['response']['venues']
+
+def getFromDict(diccionario,mapa):
+    return reduce(operator.getitem,mapa,diccionario)
+
+def type_point(lista):
+    return {"type":"Point", "coordinates": lista}
+
+def extraetodo(json):
+    todo = {"nombre": ["name"], "latitud": ["location", "lat"], "longitud": ["location", "lng"], 'category':['categories', 0 ,'name'], 'distance': ['location', 'distance']}
+    total = []
+    for elemento in json:
+        libre = {key: getFromDict(elemento, value) for key,value in todo.items()}
+        libre["location"] = type_point([libre["latitud"], libre["longitud"]])
+        total.append(libre)
+    return total
